3d565f990a42e124dfa411c4c27c08c80913aa54
dockerhopper <bransonancrawford@gmail.com>
2025-08-27 19:54:37 -0700

    new features

--- db.c
+++ db.c
@@ -28,7 +28,9 @@ typedef enum {
 typedef enum { 
 	PREPARE_SUCCESS, 
 	PREPARE_UNRECOGNIZED_STATEMENT, 
-	PREPARE_SYNTAX_ERROR
+	PREPARE_SYNTAX_ERROR,
+    PREPARE_NEGATIVE_ID,
+    PREPARE_STRING_TOO_LONG
 } PrepareResult;
 
 typedef enum {
@@ -39,8 +41,8 @@ typedef enum {
 //STRUCT
 typedef struct {
 	uint32_t id;
-	char username[COLUMN_USERNAME_SIZE];
-	char email[COLUMN_EMAIL_SIZE];
+	char username[COLUMN_USERNAME_SIZE+1];
+	char email[COLUMN_EMAIL_SIZE+1];
 } Row;
 
 typedef struct {
@@ -100,22 +102,40 @@ typedef struct {
 } Table;
 
 //FUNCTIONS
+PrepareResult prepare_insert(InputBuffer* input_buffer, Statement* statement){
+    statement->type = STATEMENT_INSERT;
+
+    char* keyword = strtok(input_buffer->buffer, " ");
+    char* id_string = strtok(NULL, " " );
+    char* username = strtok(NULL," ");
+    char* email = strtok(NULL, " ");
+
+    if(id_string == NULL || username == NULL || email == NULL){
+        return PREPARE_SYNTAX_ERROR;
+    }
+
+    int id = atoi(id_string);
+    if(id<0){
+        return PREPARE_NEGATIVE_ID;
+    }
+    if(strlen(email) > COLUMN_EMAIL_SIZE) {
+        return PREPARE_STRING_TOO_LONG;
+    }
+
+    statement->row_to_insert.id = id;
+    strcpy(statement->row_to_insert.username,username);
+    strcpy(statement->row_to_insert.email,email);
+
+    return PREPARE_SUCCESS;
+}
 PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement){
 	if(strncmp(input_buffer->buffer, "insert",6) == 0){
-		statement->type = STATEMENT_INSERT;
-        int args_assigned = sscanf(
-            input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id),
-            statement->row_to_insert.username, statement->row_to_insert.email);
-            if(args_assigned <3){
-                return PREPARE_SYNTAX_ERROR;
-            }
-		return PREPARE_SUCCESS;
-	}
-	if(strcmp(input_buffer->buffer, "select") == 0){
-		statement->type = STATEMENT_SELECT;
-		return PREPARE_SUCCESS;
-	}
-	return PREPARE_UNRECOGNIZED_STATEMENT;
+		return prepare_insert(input_buffer,statement);
+    }
+	if (strcmp(input_buffer->buffer, "select") == 0) {
+        statement->type = STATEMENT_SELECT;
+        return PREPARE_SUCCESS;
+    }
 }
 MetaCommandResult do_meta_command(InputBuffer* input_buffer){
 	if(strcmp(input_buffer->buffer, ".exit") == 0){
@@ -124,7 +144,8 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer){
 		return META_UNRECOGNIZE_COMMAND;
 	}
 }
-//Row operations converting to and from the compact represenetaion
+//Row operations, either inserting or grabbing row information at a given
+//row location
 void serialize_row(Row * source, void* destination){
 	memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
 	memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
@@ -181,9 +202,10 @@ ExecuteResult execute_insert(Statement* statement, Table* table){
 	if(table->num_rows >= TABLE_MAX_ROWS){
 		return EXECUTE_TABLE_FULL;
 	}
-
+    //Grabbing the row where we will insert
 	Row* row_to_insert = &(statement->row_to_insert);
 
+    //Placing the row in memory
 	serialize_row(row_to_insert, row_slot(table,table->num_rows));
 	table->num_rows += 1;
 
@@ -244,6 +266,12 @@ int main(int argc, char* argv[]) {
 	switch(prepare_statement(input_buffer, &statement)){
 		case(PREPARE_SUCCESS):
 			break;
+        case(PREPARE_NEGATIVE_ID):
+            printf("ID must be positive.\n");
+            break;
+        case(PREPARE_STRING_TOO_LONG):
+            printf("String is too long.\n");
+            break;
 		case(PREPARE_SYNTAX_ERROR):
 			printf("Syntax error. Could not parse statement.");
 			continue;
